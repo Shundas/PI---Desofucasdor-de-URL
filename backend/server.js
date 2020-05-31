da16dda483d64a07db2b703256e0d2506e204cb9
00crono00 <brunozsantos95@gmail.com>
2020-05-31 19:02:08 -0300

    Server.js e App.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,5 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
 const port = 3001;
-
-// app.use(express.static(__dirname + '/frontend'));
-app.use(express.json());
-app.use(cors());
-
-const routes = require('./src/routes/index');
-app.use(routes);
-
+const app = require('./src/app');
 
 app.listen(port, () => {
   console.log(`Server is running at port: ${port}`);
