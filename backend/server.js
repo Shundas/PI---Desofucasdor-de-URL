89d87c7b71222b741e24622ca50e9be7f1048048
Felipe <felipeannunziato04@gmail.com>
2020-05-31 17:08:17 -0300

    Servidor

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,15 @@ const express =  require('express');
 const app = express();
 const cors = require('cors')
 const port = 3001;
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Funcionando")
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at port: ${port}`)
+})
