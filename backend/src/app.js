9be4028e950648e90148f1947b983f0134b15948
Felipe <felipeannunziato04@gmail.com>
2020-06-02 21:26:54 -0300

    SRC

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const routes = require('./routes/routes');
 
 const app = express();
-
 const port = 3001;
 
+app.use(express.json())
 app.use(routes);
 
 app.listen(port);
