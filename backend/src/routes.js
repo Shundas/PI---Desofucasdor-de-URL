d7b071b2fed956c297c243b56ea24b7d7338947f
00crono00 <brunozsantos95@gmail.com>
2020-06-04 20:29:37 -0300

    Alteração das rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
 const express = require('express');
 
-router.get('/', (req, res) => {
+router.get('/login', (req, res) => {
     res.send('<h1>Login</h1>')
 });
 
-router.get('/:id', (req, res) => {
+router.get('/user_details/:id', (req, res) => {
     res.send(`<h1>Detalhes do usuário ${req.params.id}</h1>`);
 });
 
-router.post('/', (red, res) => {
+router.post('/new_user', (red, res) => {
     res.send('<h1>Criar usuário</h1>');
 });
 
-router.put('/:id', (red, res) => {
+router.put('/edit_user/:id', (red, res) => {
     res.send('<h1>Editar usuário</h1>');
 });
 
-router.delete('/:id', (red, res) => {
+router.delete('/delete_user/:id', (red, res) => {
     res.send('<h1>Deletar usuário</h1>');
 });
 
