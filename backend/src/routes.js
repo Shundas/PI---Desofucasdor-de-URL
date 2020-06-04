6974ec5f1432967bf70c72a0f3d29500fd871c4b
00crono00 <brunozsantos95@gmail.com>
2020-06-04 20:37:16 -0300

    Ajustes arquivo routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,16 +9,16 @@ router.get('/user_details/:id', (req, res) => {
     res.send(`<h1>Detalhes do usuário ${req.params.id}</h1>`);
 });
 
-router.post('/new_user', (red, res) => {
+router.post('/new_user', (req, res) => {
     res.send('<h1>Criar usuário</h1>');
 });
 
-router.put('/edit_user/:id', (red, res) => {
-    res.send('<h1>Editar usuário</h1>');
+router.put('/edit_user/:id', (req, res) => {
+    res.send(`<h1>Editar usuário ${req.params.id}</h1>`);
 });
 
-router.delete('/delete_user/:id', (red, res) => {
-    res.send('<h1>Deletar usuário</h1>');
+router.delete('/delete_user/:id', (req, res) => {
+    res.send(`<h1>Deletar usuário ${req.params.id}</h1>`);
 });
 
 module.exports = router;
