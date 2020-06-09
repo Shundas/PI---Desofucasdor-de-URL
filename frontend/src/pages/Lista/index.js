08dbd94c73f117b17bb39d8623ac2e4624b73f89
breno-felipe <brendo.filipe2050@gmail.com>
2020-06-09 18:10:04 -0300

    comitado

--- frontend/src/pages/Lista/index.js
+++ frontend/src/pages/Lista/index.js
@@ -1,53 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import api from '../../services/api'
-import { Link } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 export default function Lista() {
-    const [users, setUsers] = useState([{
-        id: '',
-        name: '',
-        email: ''
-    }])
+  const [users, setUsers] = useState([
+    {
+      id: "",
+      name: "",
+      email: "",
+    },
+  ]);
 
-    useEffect(() => {
-        api.get('/').then(response => {
-            setUsers(response.data)
-        })
-    },[])
+  useEffect(() => {
+    api.get("/").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
 
-    async function handleDeleteUser(id) {
+  async function handleDeleteUser(id) {
     //    alert('Você deseja realmente excluir esse usuário?'
-       await api.delete(`/${id}`);
-       alert('Usuário Deletado')
-       window.location.reload()
-    }
+    await api.delete(`/${id}`);
+    alert("Usuário Deletado");
+    window.location.reload();
+  }
 
-    return (
-        <>
-            <header>Header</header>
-            <div>
-            <table>
-                <tbody>
-                    {users.map(user => (
-                        <tr  key={user.id}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <Link to={`/editar/${user.id}`}>
-                                <button>Editar</button>
-                            </Link>
-                            <button 
-                                id={user.id} 
-                                onClick={() => handleDeleteUser(user.id)}
-                            >
-                            Excluir
-                            </button>
-                        </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <header className="header">Header</header>
+      <div className="container">
+        <table>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <Link className="tolink" to={`/editar/${user.id}`}>
+                  <button>Editar</button>
+                </Link>
+                <button
+                  className="btn"
+                  id={user.id}
+                  onClick={() => handleDeleteUser(user.id)}
+                >
+                  Excluir
+                </button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
