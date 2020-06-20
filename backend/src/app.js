8f0a2479af72f413e5508e39f3a769f859ab035f
Felipe <felipeannunziato04@gmail.com>
2020-06-20 12:05:21 -0300

    Recebendo e Enviando arquivo para download

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = require('./routes');
 const cors = require('cors')
 const knex = require('./database/index')
+const fs = require('fs');
 
 const app = express();
 const port = 3001;
@@ -14,6 +15,23 @@ app.use('/', routes);
 app.use((error, request, response, next) => {
     response.status(error.status || 500)
     response.json({ error: error.message })
+})
+
+//Diretório de saída de arquivos
+fs.access("output_files", fs.constants.F_OK, (err) => {
+    if(err) {
+        fs.mkdir("output_files", (err) => {
+            if(err) throw err;
+            console.log("output_files criado com sucesso.")
+        })
+    } else {
+        fs.stat("output_files", (err, stats) => {
+            if(err) throw err;
+            if(!stats.isDirectory()) {
+                throw new Error("output_files existe, mas não é um diretório")
+            }
+        })
+    }
 })
 
 app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`));
--- backend/src/controller/decodeController.js
+++ backend/src/controller/decodeController.js
@@ -1,21 +1,63 @@
 const multer = require('multer');
+const fs = require('fs');
+const readline = require('readline')
+const { once } = require('events'); 
 
 module.exports = {
     enviarArquivo(request, response) {
+
     const upload = multer({
-        dest: 'upload_files/'
+        dest: 'upload_files/',
+        fileFilter: (request, file, cb) => {
+            if(file.mimetype != "text/plain" && !file.mimetype.includes('log')) {
+                return cb(new Error("Formato de arquivo inválido"))
+            }
+            cb(null, true)
+        }
     }).single('attachment');
     
-    upload(request, response, (err) => {
+    upload(request, response, async (err) => {
         if (err) {
             console.log(err);
             response.status(422).send();
         } else {
             let file = request.file;
-            console.log(file);
-            response.send();
+            const path = await processFile(file)
+            if (path) {
+                response.download(path, file.originalname);
+            } else {
+                response.status(500).send()
+            }
         }
-    }) 
+    })
+    
+    async function processFile(file) {
+        const outPath = `output_files/${file.filename}`;
+        const writeStream = fs.createWriteStream(outPath, {
+            flags: 'a'
+        });
+
+        writeStream.on("error", (err) => {
+            console.log(err);
+            throw err;
+        });
+
+        const readInterface = readline.createInterface({
+            input: fs.createReadStream(file.path)
+        });
+        
+        readInterface.on('line', (line) => {
+            writeStream.write(`${line.toUpperCase()}\n`)
+        });
+
+        readInterface.on('close', () => {
+            writeStream.end();
+        });
+
+        await once(readInterface, 'close');
+
+        return outPath;
+    }
   }
 }
 
